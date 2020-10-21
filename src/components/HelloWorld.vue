835a881b253e2a88249a4d65bb4cb2cade72ca09
SaarAvraham <av.saar93@gmail.com>
2020-10-21 11:11:26 +0300

    date-range-picker

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,244 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1 class="marDown" > {{ msg }}</h1>
+<!--        <div class="centerTh">-->
+        <!--            <progress-bar-->
+        <!--                    :options="options"-->
+        <!--                    :value="injectionProgress"-->
+        <!--            />-->
+        <!--        </div>-->
+        <b-form >
+            <b-form-group
+                    id="input-group-1"
+                    label="Number of calls:"
+                    label-for="input-1"
+            >
+                <b-form-input
+                        id="input-1"
+                        v-model="form.email"
+                        type="number"
+                        required
+                        placeholder="Enter a number of calls to inject"
+                ></b-form-input>
+            </b-form-group>
+        </b-form>
+        <month-picker-input class="centerTh1" @change="saveDate" :range="true" v-show="false"></month-picker-input>
+        <date-range-picker
+                v-model="dateRange"
+                @update="updateDate"
+        >
+            <template v-slot:input="picker" style="min-width: 350px;">
+                {{ picker.startDate}} - {{ picker.endDate}}
+            </template>
+            <div>sdknsakdsankdsan</div>
+        </date-range-picker>
+        <b-progress :max="max" class="mb-3 centerTh" >
+            <b-progress-bar :value="injectionProgress" :label="`${injectionProgress}%`"></b-progress-bar>
+        </b-progress>
+
+
+        <!--        <month-picker-input @change="saveDate" :range="true"></month-picker-input>-->
+        <!--    <month-picker-input @change="saveToDate"></month-picker-input>-->
+        <b-button variant="outline-primary" @click="sendInjectRequest()">Inject</b-button>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import {MonthPickerInput} from 'vue-month-picker'
+    import axios from 'axios'
+    // import ProgressBar from 'vuejs-progress-bar'
+    // import * as https from "https";
+    import DateRangePicker from 'vue2-daterange-picker'
+    import SockJS from "sockjs-client"
+    import Stomp from "webstomp-client"
+    import 'bootstrap/dist/css/bootstrap.css'
+    import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        },
+        components: {
+            MonthPickerInput,DateRangePicker
+            // ProgressBar
+        },
+        data: function () {
+            return {
+                dateRange: {
+                    startDate: undefined,
+                    endDate: undefined
+                },
+                form: {
+                    callsToInject: undefined                },
+                types: [
+                    'number'
+                ],
+
+                injectionProgress: 0,
+                max: 100,
+                stompClient: undefined,
+                connected: false,
+                date: {
+                    rangeFromMonth: null,
+                    rangeToMonth: null,
+                    year: null
+                },
+                options: {
+                    text: {
+                        color: '#FFFFFF',
+                        shadowEnable: true,
+                        shadowColor: '#000000',
+                        fontSize: 14,
+                        fontFamily: 'Helvetica',
+                        dynamicPosition: false,
+                        hideText: false
+                    },
+                    progress: {
+                        color: '#2dbd2d',
+                        backgroundColor: '#333333'
+                    },
+                    layout: {
+                        height: 35,
+                        width: 640,
+                        verticalTextAlign: 61,
+                        horizontalTextAlign: 43,
+                        zeroOffset: 0,
+                        strokeWidth: 30,
+                        progressPadding: 0,
+                        type: 'line'
+                    }
+                }
+                // fromDate: {
+                //     from: null,
+                //     to: null,
+                //     month: null,
+                //     year: null
+                // },
+                // toDate: {
+                //     from: null,
+                //     to: null,
+                //     month: null,
+                //     year: null
+                // }
+            }
+        },
+        methods: {
+            updateDate: function(date) {
+                this.dateRange.startDate = date.startDate
+                this.dateRange.endDate = date.endDate
+            },
+            sendInjectRequest: function () {
+                // const agent = https.Agent({
+                //     rejectUnauthorized: false
+                // });
+
+                axios.post('http://localhost:9090/calls', this.date)
+                    .then(response => (console.log(response.data)))
+            },
+            saveDate: function (date) {
+                this.date.rangeFromMonth = date.rangeFrom
+                this.date.rangeToMonth = date.rangeTo
+                this.date.year = date.year
+                console.log('Current date is:')
+                console.log(this.date)
+            }
+            // saveFromDate: function (date) {
+            //   console.log(date)
+            //   this.fromDate = date
+            // },
+            // saveToDate: function (date) {
+            //   console.log(date)
+            //   this.toDate = date
+            // }
+        },
+        created: function () {
+            // console.log("Starting connection to WebSocket Server")
+            // this.connection = new WebSocket('ws://localhost:9090/chat')
+            //
+            // this.connection.onmessage = function (event) {
+            //     console.log(event);
+            // }
+            //
+            // this.connection.onopen = function (event) {
+            //     console.log(event)
+            //     console.log("Successfully connected to the echo websocket server...")
+            // }
+            const socket = new SockJS("http://localhost:9090/updates");
+            this.stompClient = Stomp.over(socket);
+            this.stompClient.connect(
+                {},
+                frame => {
+                    console.log(frame);
+                    this.connected = true;
+                    this.stompClient.subscribe("/topic/messages", message => {
+                        console.log(message);
+                        console.log(message.body.injectionProgress);
+                        const body = JSON.parse(message.body);
+                        this.injectionProgress = body.injectionProgress
+                        this.callsInjected = body.callsInjected
+                        this.remainingSeconds = body.remainingSeconds
+                        // this.received_messages.push(JSON.parse(message.body).content);
+                    });
+                },
+                error => {
+                    console.log(error);
+                    this.connected = false;
+                }
+            );
+        }
+        // disconnect() {
+        //     if (this.stompClient) {
+        //         this.stompClient.disconnect();
+        //     }
+        //     this.connected = false;
+        // },
+        // tickleConnection() {
+        //     this.connected ? this.disconnect() : this.connect();
+        // }
+        // }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    .marDown {
+        margin-bottom: 70px;
+    }
+
+    .centerTh {
+        margin-top: 280px;
+
+    }
+
+    .centerTh1 {
+        width: 50%;
+        margin: 0 auto;
+    }
+
+    .month-pickerEnrich {
+        padding-top: 50px;
+        padding-right: 30px;
+        padding-bottom: 480px;
+        padding-left: 80px;
+    }
+
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
